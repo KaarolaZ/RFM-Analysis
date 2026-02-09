5b01fd65b26167e532ff7a3e763d2ed2b49d1980
Karola_Z <z.karolina174@gmail.com>
2026-02-09 15:32:41 +0100

    utworzenie wyczyszczonego widoku

--- rfm_cleaning.sql
+++ rfm_cleaning.sql
@@ -1,1 +1,17 @@
 SELECT * FROM rfm;
+ALTER TABLE rfm
+RENAME COLUMN "invoice_data" TO invoice_date;
+--ocyzszczony widok
+CREATE OR REPLACE VIEW online_retail AS 
+SELECT
+    customer_id,
+    invoice,
+    to_timestamp(invoice_date, 'MM/DD/YY HH24:MI'),   --problemy z odczytniem formatu daty, dlatego musimy wskazać konkretny format
+    (CAST(quantity AS INT)* CAST(price AS NUMERIC)) AS total_price
+    FROM rfm
+    WHERE CAST(quantity AS INT)>0
+        AND customer_id IS NOT NULL
+        AND CAST(price AS NUMERIC) >0
+
+SELECT * FROM online_retail;
+
